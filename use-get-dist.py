4925f6cf930d620da76c94d7335d15ec75e7e0f7
tgbar <tmbarreiro@gmail.com>
2024-07-10 15:47:14 +0100

    stuff

--- use-get-dist.py
+++ use-get-dist.py
@@ -5,17 +5,20 @@ from getdist import MCSamples, plots
 
 # np.save("myname.npy", samples)
 samples = np.load("sample-test.npy")
-labels = ["Om", "h"]
+labels = ["Om", "Mscr"]
 
 # use getdist
 #
 samples_g = MCSamples(samples=samples, names=labels, labels=labels, settings=dict(smooth_scale_2D=3))
 
 g = plots.get_subplot_plotter()
-g.triangle_plot([samples_g], filled=True, param_limits={'h':(10.0,30.0),'Om':(0.1,0.4)})
+g.triangle_plot([samples_g], filled=True, param_limits={'Mscr':(10.0,30.0),'Om':(0.1,0.4)})
 
-print(samples_g.getInlineLatex('h', limit=1))
+print(samples_g.getInlineLatex('Mscr', limit=1))
 print(samples_g.getInlineLatex('Om', limit=1))
 
 g2 = plots.get_single_plotter()
 g2.plot_1d(samples_g,'Om')
+
+g3 = plots.get_single_plotter()
+g3.plot_2d(samples_g,param_pair=['Om','Mscr'],filled=False, lims=[0.1, 0.5, 18,25])
